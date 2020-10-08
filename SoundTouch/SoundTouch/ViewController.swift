c1818ba99f16402dbfd52a3e83a6d77db47ca080
Rabbit <dimkazayats@gmail.com>
2020-10-08 10:39:09 +0300

    Добавил проект

---
+++ SoundTouch/SoundTouch/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SoundTouch
+//
+//  Created by Admin on 10/8/20.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
